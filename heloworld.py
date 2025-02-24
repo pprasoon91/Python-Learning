bf7a23fd5b39efbca79801d18063dab1d0c46811
Piyush Prasoon <pppiyushprasoon@gmail.com>
2025-02-24 14:29:25 +0530

    added 2 more print types

--- heloworld.py
+++ heloworld.py
@@ -3,3 +3,5 @@ print ('single quote "Hello world"')
 print ('''Multi
 line
 'Hello' "World"''')
+print('Hello World without space between print and the brackets')
+print("Hello World - this print statement is ending with semicolon");
